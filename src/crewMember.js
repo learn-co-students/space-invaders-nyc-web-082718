51d9803ba22c71dedee57469055e5017789325b9
sinyoungnam <sinnam90@gmail.com>
2018-10-11 15:43:54 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,29 @@
-class CrewMember {}
+class CrewMember {
+ constructor(position, currentShip) {
+   this.position = position,
+   this.currentShip = "Looking for a Rig"
+ }
+
+ engageWarpDrive() {
+   if (this.position === "Pilot" && this.currentShip === "Looking for a Rig") {
+     return 'had no effect'
+   }
+ }
+
+ setsInvisibility() {
+   if (this.position === "Defender" && this.currentShip === "Looking for a Rig") {
+     return 'had no effect'
+   } else if (this.position === "Gunner") {
+     this.currentShip.cloaked = false
+   } else if (this.position === "Defender") {
+     this.currentShip.cloaked = true
+   }
+ }
+
+ chargePhasers() {
+   if (this.position === "Gunner" && this.currentShip === "Looking for a Rig") {
+     return 'had no effect'
+   }
+ }
+
+}
