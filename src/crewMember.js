d4485399f8f655a06ae9945a505476962654c42e
miriamfrank23 <miriamfrank23@gmail.com>
2018-10-11 14:39:11 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,37 @@
-class CrewMember {}
+let CrewMemberId = 0
+class CrewMember {
+
+    constructor(position) {
+      this.position = position
+      this.id = ++CrewMemberId
+      this.currentShip = 'Looking for a Rig'
+      }
+
+  setsInvisibility() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect'
+    } else {
+      if (this.position === 'Defender'){
+        this.currentShip.cloaked = true
+      }
+    }
+  }
+
+  engageWarpDrive() {
+   if (this.currentShip === 'Looking for a Rig') {
+     return 'had no effect'
+   } else if (this.position === 'Pilot') {
+     this.currentShip.warpDrive = 'engaged'
+   }
+ }
+
+  chargePhasers() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect'
+    } else if (this.position === 'Gunner') {
+      this.currentShip.phasersCharge = 'charged'
+    }
+  }
+
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,25 @@
-class Spaceship {}
+let spaceShipId = 0
+class Spaceship {
+
+  constructor(name, crew = [], phasers, shields) {
+    this.name = name
+    this.crew = crew
+    if (this.crew.length === 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+      for (let crewMemberIndex in crew) {
+        crew[crewMemberIndex].currentShip = this
+      }
+    }
+    this.phasers = phasers
+    this.shields = shields
+    this.id = ++spaceShipId
+    this.cloaked = false
+    this.warpDrive='disengaged'
+    this.phasersCharge='uncharged'
+  }
+
+
+
+}
